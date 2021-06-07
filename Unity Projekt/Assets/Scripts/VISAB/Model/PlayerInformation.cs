3f8ad2615fbfefaeb40182226b7413f48dae18f8
leRoe93 <leon.roemer@icloud.com>
2021-06-07 19:03:37 +0200

    Initial models for player info and statistics analogous to shooter.

---
+++ Unity Projekt/Assets/Scripts/VISAB/Model/PlayerInformation.cs
@@ -0,0 +1,33 @@
+namespace Assets.Scripts.VISAB
+{
+    public class PlayerInformation
+    {
+        public int Brick { get; set; }
+
+        public bool FreeBuild { get; set; }
+
+        public bool FreeBuildRoad { get; set; }
+
+        public bool HasLongestRoad { get; set; }
+
+        public bool IsAi { get; set; }
+
+        public int LongestRoad { get; set; }
+
+        public string Name { get; set; }
+
+        public string Plan { get; set; }
+
+        public float RoadRange { get; set; }
+
+        public int Sheep { get; set; }
+
+        public int Stone { get; set; }
+
+        public int VictoryPoints { get; set; }
+
+        public int Wheat { get; set; }
+
+        public int Wood { get; set; }
+    }
+}
